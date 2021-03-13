2f5c40cce5b9162da31d933088adea77124a9812
Samridhi-88 <80439205+Samridhi-88@users.noreply.github.com>
2021-03-13 18:42:52 +0530

    Add files via upload

---
+++ bithaut_multiplication_multipal.py
@@ -0,0 +1,10 @@
+a=int(input("enter the num="))
+i=0
+while i<(a):
+    num=(input("inter the num=="))
+    j=0
+    while j<len(num):
+        print(int(num[0])+int(num[-1]))
+        break
+        j+=1
+    i+=1
